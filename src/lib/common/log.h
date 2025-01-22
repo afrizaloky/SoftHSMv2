d919756b5bfa570fc72b198b98b0ebf30b3ee9bc
afrizaloky <afrizaloky@outlook.com>
2025-01-22 09:04:25 +0700

    change path

--- src/lib/common/log.h
+++ src/lib/common/log.h
@@ -100,7 +100,7 @@ inline void logMessage(Args... args) {
     ((oss << args << ' '), ...); // Append space after each argument
 
     // Open the log file in append mode
-    std::ofstream logFile("/tmp/log.txt", std::ios::app);
+    std::ofstream logFile("/var/log/softhsmkk.txt", std::ios::app);
     
     // Check if the file is open
     if (logFile.is_open()) {
