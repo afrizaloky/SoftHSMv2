c2744c388bc85e3101542d12f80ade13c7de1ac7
afrizaloky <afrizaloky@outlook.com>
2025-02-10 16:18:09 +0700

    change log

--- src/lib/SoftHSM.cpp
+++ src/lib/SoftHSM.cpp
@@ -2209,8 +2209,9 @@ CK_RV SoftHSM::SymEncryptInit(CK_SESSION_HANDLE hSession, CK_MECHANISM_PTR pMech
 
 	// Check the key handle.
 	OSObject *key = (OSObject *)handleManager->getObject(hKey);
-	logMessage(fmt::format("SoftHSM::SymEncryptInit | attribute: {}", key->getAttribute(CKA_ID).getByteStringValue().hex_str()));
+	logMessage(fmt::format("SoftHSM::SymEncryptInit | attribute: {}", key->getByteStringValue(CKA_LABEL).hex_str()));
 	if (key == NULL_PTR || !key->isValid()) return CKR_OBJECT_HANDLE_INVALID;
+
 
 	CK_BBOOL isOnToken = key->getBooleanValue(CKA_TOKEN, false);
 	CK_BBOOL isPrivate = key->getBooleanValue(CKA_PRIVATE, true);
